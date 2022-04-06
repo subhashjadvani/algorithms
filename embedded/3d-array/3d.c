5adf8207dad9c83227d739f33c22b1ee61a88215
Subhash Jadavani <subhashjadvani@gmail.com>
2022-04-05 22:16:27 -0700

    temp
    Signed-off-by: Subhash Jadavani <subhashjadvani@gmail.com>

--- embedded/3d-array/3d.c
+++ embedded/3d-array/3d.c
@@ -4,24 +4,25 @@
 
 int main(int argc, char* argv[])
 {
+	int n = 10;
 	int ***ptr;
 
-	ptr = (int ***) malloc(sizeof(int ***) * 10);
+	ptr = (int ***) malloc(sizeof(int ***) * n);
 
-	for (int i = 0; i < 10; i++) {
-		ptr[i] = (int**)malloc(sizeof(int **) * 10);
+	for (int i = 0; i < n; i++) {
+		ptr[i] = (int**)malloc(sizeof(int **) * n);
 
-		for (int j = 0; j < 10; j++) {
-			ptr[i][j] = (int*)malloc(sizeof(int *) * 10);
+		for (int j = 0; j < n; j++) {
+			ptr[i][j] = (int*)malloc(sizeof(int) * n);
 		
-			for (int k = 0; k < 10; k++)
+			for (int k = 0; k < n; k++)
 				ptr[i][j][k] = i * 100 + j * 10 + k;
 		}
 	}
 
-	for (int i = 0; i < 10; i++) {
-		for (int j = 0; j < 10; j++) {
-			for (int k = 0; k < 10; k++)
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			for (int k = 0; k < n; k++)
 				printf("ptr[%d][%d][%d] %d\n", i, j, k, ptr[i][j][k]);
 		}
 	}
