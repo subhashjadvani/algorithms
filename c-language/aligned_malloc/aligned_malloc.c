79d71056b26a9f46cac410ca0d7c8c8f7bb19ac9
Subhash Jadavani <subhashjadvani@gmail.com>
2022-06-17 12:23:45 -0700

    aligned_malloc

---
+++ c-language/aligned_malloc/aligned_malloc.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void* aligned_malloc(size_t bytes, size_t alignment)
+{
+	if ((alignment & (alignment - 1)) != 0)
+		return NULL;
+
+	uintptr_t p1, p2;
+	size_t ptr_size = sizeof(uintptr_t);
+    
+	p1 = (uintptr_t) malloc(bytes + alignment + ptr_size);
+	if (p1 == 0)
+		return NULL;
+
+	printf("malloc address: %p\n", (void*) p1);
+    
+	uintptr_t addr = (uintptr_t) p1 + alignment + ptr_size;
+	p2 = addr - (addr % alignment);
+    
+	//*((uintptr_t*)p2 - 1) = p1;
+	*(uintptr_t*)(p2 - ptr_size) = p1;
+    
+	return (void*) p2;
+}
+void aligned_free(void* p)
+{
+    free ((void*)(*((size_t*)p - 1)));
+}
+int main(void) {
+    void *address = aligned_malloc(100, 8);
+
+    printf("aligned address: %p\n", address);
+
+    return 0;
+}
