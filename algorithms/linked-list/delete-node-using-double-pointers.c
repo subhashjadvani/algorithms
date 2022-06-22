32cfc138005bc6dcbbecfd324f9d4797c753b9ef
Subhash Jadavani <subhashjadvani@gmail.com>
2022-06-22 08:44:28 -0700

    update

--- algorithms/linked-list/delete-node-using-double-pointers.c
+++ algorithms/linked-list/delete-node-using-double-pointers.c
@@ -1,13 +1,15 @@
 // Sources: https://stackoverflow.com/questions/12914917/using-pointers-to-remove-item-from-singly-linked-list
+// https://www.reddit.com/r/HandmadeQuake/comments/4d6993/linus_torvaldss_double_pointer_problem/?utm_medium=android_app&utm_source=share
 
 typedef struct nodetype
 {
     struct nodetype * next;
-}node;
+} node;
+
 typedef struct listType
 {
     struct nodetype * head;
-}list;
+} list;
 
 void remove_node(list * l, node * n)
 {
